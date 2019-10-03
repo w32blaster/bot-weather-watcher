dc5318fd2da5ce2bfd180dc25de5c840a1a713b8
Ilja Hamalainen <ilja.hamalainen@infinityworks.com>
2019-10-03 18:11:19 +0100

    Add more unit tests

--- command/state.machine_test.go
+++ command/state.machine_test.go
@@ -13,6 +13,7 @@ import (
 const (
 	TestLocationID = "111"
 	UserID         = 111
+	User2ID        = 222
 )
 
 func TestStateMachineStepByStep(t *testing.T) {
@@ -70,6 +71,115 @@ func TestStateMachineStepByStep(t *testing.T) {
 	assert.Equal(t, 20, bookmarks[0].MaxWindSpeed)
 	assert.Equal(t, 10, bookmarks[0].LowestTemp)
 	assert.True(t, bookmarks[0].IsReady)
+}
+
+// scenario: two users, two bookmarks
+func TestStateMachineForTwoUsers(t *testing.T) {
+
+	// Given:
+	dir, db := prepareDB()
+	defer os.RemoveAll(dir)
+	defer db.Close()
+
+	// State machine flow for user 1
+	sm, _ := LoadStateMachineFor(UserID, db)
+	sm.CreateNewBookmark()
+	assert.Equal(t, StepEnterLocation, sm.currentState)
+
+	sm.ProcessNextState(LocationIDPrefix + TestLocationID)
+	assert.Equal(t, StepEnterMaxWindSpeed, sm.currentState)
+
+	sm.ProcessNextState("20")
+	assert.Equal(t, StepEnterMinTemp, sm.currentState)
+
+	sm.ProcessNextState("10")
+	assert.Equal(t, FINISHED, sm.currentState)
+
+	// Repeat the same for User 2
+	sm, _ = LoadStateMachineFor(User2ID, db)
+	sm.CreateNewBookmark()
+	assert.Equal(t, StepEnterLocation, sm.currentState)
+
+	sm.ProcessNextState(LocationIDPrefix + TestLocationID)
+	assert.Equal(t, StepEnterMaxWindSpeed, sm.currentState)
+
+	sm.ProcessNextState("30")
+	assert.Equal(t, StepEnterMinTemp, sm.currentState)
+
+	sm.ProcessNextState("5")
+	assert.Equal(t, FINISHED, sm.currentState)
+
+	// make sure we have two bookmarks in the database
+	var bookmarks []structs.UsersLocationBookmark
+	assert.Nil(t, db.All(&bookmarks))
+	assert.Equal(t, 2, len(bookmarks))
+
+	// and the bookmark contains valid information
+	assert.Equal(t, TestLocationID, bookmarks[0].LocationID)
+	assert.Equal(t, UserID, bookmarks[0].UserID)
+	assert.Equal(t, 20, bookmarks[0].MaxWindSpeed)
+	assert.Equal(t, 10, bookmarks[0].LowestTemp)
+	assert.True(t, bookmarks[0].IsReady)
+
+	// and the bookmark contains valid information
+	assert.Equal(t, TestLocationID, bookmarks[1].LocationID)
+	assert.Equal(t, User2ID, bookmarks[1].UserID)
+	assert.Equal(t, 30, bookmarks[1].MaxWindSpeed)
+	assert.Equal(t, 5, bookmarks[1].LowestTemp)
+	assert.True(t, bookmarks[1].IsReady)
+}
+
+// scenario: two users, two bookmarks
+func TestStateMachineForTwoUsersNotFinished(t *testing.T) {
+
+	// Given:
+	dir, db := prepareDB()
+	defer os.RemoveAll(dir)
+	defer db.Close()
+
+	// State machine flow for user 1
+	sm, _ := LoadStateMachineFor(UserID, db)
+	sm.CreateNewBookmark()
+	assert.Equal(t, StepEnterLocation, sm.currentState)
+
+	sm.ProcessNextState(LocationIDPrefix + TestLocationID)
+	assert.Equal(t, StepEnterMaxWindSpeed, sm.currentState)
+
+	sm.ProcessNextState("20")
+	assert.Equal(t, StepEnterMinTemp, sm.currentState)
+
+	sm.ProcessNextState("10")
+	assert.Equal(t, FINISHED, sm.currentState)
+
+	// Repeat the same for User 2
+	sm, _ = LoadStateMachineFor(User2ID, db)
+	sm.CreateNewBookmark()
+	assert.Equal(t, StepEnterLocation, sm.currentState)
+
+	sm.ProcessNextState(LocationIDPrefix + TestLocationID)
+	assert.Equal(t, StepEnterMaxWindSpeed, sm.currentState)
+
+	sm.ProcessNextState("30")
+	assert.Equal(t, StepEnterMinTemp, sm.currentState)
+
+	// make sure we have two bookmarks in the database
+	var bookmarks []structs.UsersLocationBookmark
+	assert.Nil(t, db.All(&bookmarks))
+	assert.Equal(t, 2, len(bookmarks))
+
+	// but the first bookmark is ready
+	assert.Equal(t, TestLocationID, bookmarks[0].LocationID)
+	assert.Equal(t, UserID, bookmarks[0].UserID)
+	assert.Equal(t, 20, bookmarks[0].MaxWindSpeed)
+	assert.Equal(t, 10, bookmarks[0].LowestTemp)
+	assert.True(t, bookmarks[0].IsReady)
+
+	// ...and the second is not
+	assert.Equal(t, TestLocationID, bookmarks[1].LocationID)
+	assert.Equal(t, User2ID, bookmarks[1].UserID)
+	assert.Equal(t, 30, bookmarks[1].MaxWindSpeed)
+	assert.Equal(t, 0, bookmarks[1].LowestTemp) // default value, not filled yet
+	assert.False(t, bookmarks[1].IsReady)       // <- False!
 }
 
 func prepareDB() (string, *storm.DB) {
